dda4c90cd2322a4a3bfcef815c0039f303a786d9
junyi <qiongzhetong@163.com>
2018-09-06 14:16:34 +0800

    Create git_usage.mk

---
+++ git_usage.mk
@@ -0,0 +1,7 @@
+git查看远程仓库地址
+git remote -v
+```
+➜  systemtap git:(master) git remote -v
+origin	git://sourceware.org/git/systemtap.git (fetch)
+origin	git://sourceware.org/git/systemtap.git (push)
+```
